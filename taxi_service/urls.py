8060615b172d12b8b67bf7da73ca6b108fb18423
Ihor Zasimenko <boxletter2011@gmail.com>
2026-02-05 19:26:08 +0200

    Fix imports and line length in urls.py

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -1,3 +1,11 @@
+"""taxi_service URL Configuration"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("taxi.urls", "taxi"), namespace="taxi")),
